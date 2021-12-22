f0e54e73a04c3c669cfc8e454533c59db58bc939
Michał Listek <michal.listex@gmail.com>
2021-12-22 17:23:03 +0100

    created router links and new method to find products by category id

--- angular-ecommerce/src/app/services/product.service.ts
+++ angular-ecommerce/src/app/services/product.service.ts
@@ -12,7 +12,10 @@ export class ProductService {
   private baseUrl = 'http://localhost:8080/api/products';
   constructor(private httpClient: HttpClient) { }
 
-  getProductList(): Observable<Product[]> {
+  getProductList(theCategoryId: number): Observable<Product[]> {
+
+    // @TODO: need to build URL based on category id ... will come back to this!
+
     return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
       map(response => response._embedded.products)
     );
