f51841728d53b8960be20f4022589f082f6e7db6
Michał Listek <michal.listex@gmail.com>
2022-02-15 20:10:38 +0100

    fixed pagination not displaying

--- angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -92,11 +92,11 @@ export class ProductListComponent implements OnInit {
   }
 
   processResult() {
-    return data => {
+    return (data:any) => {
       this.products = data._embedded.products;
       this.thePageNumber = data.page.number + 1;
       this.thePageSize = data.page.size;
-      this.theTotalElements = data.page.theTotalElements;
+      this.theTotalElements = data.page.totalElements;
     };
   }
 }
